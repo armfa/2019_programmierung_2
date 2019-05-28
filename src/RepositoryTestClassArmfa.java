c232a6c8533c665e0f315c4edba722e78c33f953
Fabrice Armbruster <Fabrice Armbruster@DESKTOP-946CCGU>
2019-05-28 15:04:59 +0200

    Test commit for first installation

---
+++ src/RepositoryTestClassArmfa.java
@@ -0,0 +1,11 @@
+
+/**
+ * This Class is a test and will help you selecting the "Import existing java project" in the eclipse git wizard!
+ * @author Fabrice Armbruster
+ *
+ *
+ 
+ */
+public class RepositoryTestClassArmfa {
+
+}
